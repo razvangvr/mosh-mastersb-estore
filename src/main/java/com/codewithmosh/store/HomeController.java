46aa8ab351a6eb5d17c63b2b072a7f31e3523265
razvan.gaston <razvan.gaston@avios.com>
2025-02-27 17:32:01 +0200

    [courses | codewithmosh | SB] application.properties

--- src/main/java/com/codewithmosh/store/HomeController.java
+++ src/main/java/com/codewithmosh/store/HomeController.java
@@ -1,5 +1,6 @@
 package com.codewithmosh.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,9 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+    @Value("${spring.application.name}")
+    private  String appName;
 
     @RequestMapping("/")
     public String index() {
+        System.out.println(appName);
         return "index.html";
     }
 }
