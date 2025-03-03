5ed8c0101cea0e8b2999af6d45c489e964556c0b
razvan.gaston <razvan.gaston@avios.com>
2025-03-03 16:55:18 +0200

    [courses | codewithmosh | SB] Autowired Annotation

--- src/main/java/com/codewithmosh/store/OrderService.java
+++ src/main/java/com/codewithmosh/store/OrderService.java
@@ -1,13 +1,27 @@
 package com.codewithmosh.store;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
 
-    private final PaymentService paymentService;
+    private  PaymentService paymentService;
 
+    /**
+     * If you have Multiple Constructors AutoWired is required
+     * */
+    public OrderService() {
+    }
+
+    /**
+     * In Older Versions of Spring, we had to tell Spring
+     * To AutoWire the Objects with its dependencies.
+     * But This is no longer necessary if our Class has a Single
+     * Constructor
+     * */
+    @Autowired
     public OrderService(PaymentService paymentService, PaymentService paymentService1) {
         this.paymentService = paymentService1;
     }
