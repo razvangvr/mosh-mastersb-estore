5ed7b9a3c740973834d4e983a800b630105e6f43
razvan.gaston <razvan.gaston@avios.com>
2025-03-03 17:41:57 +0200

    [courses | codewithmosh | SB] properties files

--- src/main/java/com/codewithmosh/store/StripePaymentService.java
+++ src/main/java/com/codewithmosh/store/StripePaymentService.java
@@ -1,9 +1,14 @@
 package com.codewithmosh.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StripePaymentService implements PaymentService {
+
+
+    @Value("${stripe.api.enabled}")
+    private boolean enabled;
 
     @Override
     public void processPayment(double amount) {
