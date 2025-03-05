cdef8bdbbba7a362805f3df6f96fda36e740dccf
razvan.gaston <razvan.gaston@avios.com>
2025-03-05 13:52:08 +0200

    [courses | codewithmosh | SB]  User Entity

---
+++ src/main/java/com/codewithmosh/store/entities/User.java
@@ -0,0 +1,28 @@
+package com.codewithmosh.store.entities;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "name")
+    private String name;
+
+    @Column(nullable = false, name = "email")
+    private String email;
+
+    @Column(nullable = false, name = "password")
+    private String password;
+
+}
