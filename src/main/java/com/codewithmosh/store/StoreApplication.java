2e13ab6e7ff0af299adff3e47b707204a40dc572
razvan.gaston <razvan.gaston@avios.com>
2025-03-05 15:07:40 +0200

    [courses | codewithmosh | SB]  using Lombok

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -1,5 +1,6 @@
 package com.codewithmosh.store;
 
+import com.codewithmosh.store.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,8 +9,17 @@ import org.springframework.context.ApplicationContext;
 public class StoreApplication {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = SpringApplication.run(StoreApplication.class, args);
+//        ApplicationContext applicationContext = SpringApplication.run(StoreApplication.class, args);
 
+        var user = new User(1L, "John", "me@me.com", "password");
+        /*var user = new User();*/
+        user.setName("John");
+
+        User user2 = User.builder()
+                .name("John")
+                .email("me@me.com")
+                .password("password")
+                .build();
     }
 
 }
--- src/main/java/com/codewithmosh/store/entities/User.java
+++ src/main/java/com/codewithmosh/store/entities/User.java
@@ -7,9 +7,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Getter
 @Setter
 @Entity
