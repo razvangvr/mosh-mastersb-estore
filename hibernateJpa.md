a224f5d07c3b18751203929c597ab20bc0d45a36
razvan.gaston <razvan.gaston@avios.com>
2025-03-24 13:20:59 +0200

    [courses | codewithmosh | SB]  show Hibernate queries in Log

---
+++ hibernateJpa.md
@@ -0,0 +1,4 @@
+We see the Queries Hibernate sends to the DB
+```
+Hibernate: insert into users (email,name,password) values (?,?,?)
+```
--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -2,6 +2,7 @@ package com.codewithmosh.store;
 
 import com.codewithmosh.store.entities.Profile;
 import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,11 +13,16 @@ public class StoreApplication {
     public static void main(String[] args) {
         ApplicationContext applicationContext = SpringApplication.run(StoreApplication.class, args);
 
-        var user = User.builder()
-                .name("John")
-                .email("me@me.com")
-                .password("password")
-                .build();
+        var userRepository = applicationContext.getBean(UserRepository.class);
+
+        var user = basicUser();
+
+        userRepository.save(user);
+
+    }
+
+    private static void playWithUser() {
+        var user = basicUser();
 
         user.addTag("tag1");
 
@@ -28,6 +34,15 @@ public class StoreApplication {
         profile.setUser(user);
 
         System.out.println(user);
+    }
+
+    private static User basicUser() {
+        var user = User.builder()
+                .name("John")
+                .email("me@me.com")
+                .password("password")
+                .build();
+        return user;
     }
 
 }
