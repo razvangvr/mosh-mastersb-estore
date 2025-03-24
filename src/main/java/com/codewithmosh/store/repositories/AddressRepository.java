c284a2b5c7240572230d3813f6321758ad3997d1
razvan.gaston <razvan.gaston@avios.com>
2025-03-24 13:09:50 +0200

    [courses | codewithmosh | SB]  User & Address Repos

---
+++ src/main/java/com/codewithmosh/store/repositories/AddressRepository.java
@@ -0,0 +1,7 @@
+package com.codewithmosh.store.repositories;
+
+import com.codewithmosh.store.entities.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
