cca667b869758303679e19c91a637139f05bd290
razvan.gaston <razvan.gaston@avios.com>
2025-04-23 17:11:30 +0300

    [courses | codewithmosh | SB] ORDER BY user.Email

--- src/main/java/com/codewithmosh/store/repositories/ProfileRepository.java
+++ src/main/java/com/codewithmosh/store/repositories/ProfileRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 public interface ProfileRepository extends CrudRepository<Profile, Long> {
     @EntityGraph(attributePaths = "user")
-    @Query("select p  from Profile p where p.loyaltyPoints >= :loyaltyVal")
+    @Query("select p from Profile p where p.loyaltyPoints >= :loyaltyVal order by p.user.email")
     List<Profile> findProfilesByLoyaltyPointsGreaterThan(@Param("loyaltyVal") Integer loyaltyVal);
 }
