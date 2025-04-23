08ad1b3c7ae751d8683a210eccdbdaf4db458313
razvan.gaston <razvan.gaston@avios.com>
2025-04-23 17:08:03 +0300

    [courses | codewithmosh | SB] n+1 Problem - Entity Graph

--- src/main/java/com/codewithmosh/store/repositories/ProfileRepository.java
+++ src/main/java/com/codewithmosh/store/repositories/ProfileRepository.java
@@ -1,6 +1,7 @@
 package com.codewithmosh.store.repositories;
 
 import com.codewithmosh.store.entities.Profile;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -8,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ProfileRepository extends CrudRepository<Profile, Long> {
-    @Query("select p, p.user from Profile p where p.loyaltyPoints >= :loyaltyVal")
+    @EntityGraph(attributePaths = "user")
+    @Query("select p  from Profile p where p.loyaltyPoints >= :loyaltyVal")
     List<Profile> findProfilesByLoyaltyPointsGreaterThan(@Param("loyaltyVal") Integer loyaltyVal);
 }
