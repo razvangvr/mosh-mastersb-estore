9de6e4f973ead2b779fae2b308b126daef168028
razvan.gaston <razvan.gaston@avios.com>
2025-06-27 16:06:50 +0300

    [courses | codewithmosh | SB | REST-API | Users Controller] getUserBy ID Make it RESTFul Standards, Return 404 for User Not Found

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.codewithmosh.store.controllers;
 import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +24,13 @@ public class UserController {
 
 
     @GetMapping("/{id}")
-    public User getUser(@PathVariable long id) {
-        return userRepository.findById(id).orElse(null);
+    public ResponseEntity<User> getUser(@PathVariable long id) {
+        var user = userRepository.findById(id).orElse(null);
+
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(user);
+        }
     }
 }
