5cb883601898adbfe77faec4034779d188811c18
razvan.gaston <razvan.gaston@avios.com>
2025-06-27 15:59:03 +0300

    [courses | codewithmosh | SB | REST-API | Users Controller] getUserBy ID

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -4,17 +4,26 @@ import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserRepository userRepository;
 
 
-    @GetMapping("/users")
+    @GetMapping()
     public Iterable<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable long id) {
+        return userRepository.findById(id).orElse(null);
     }
 }
